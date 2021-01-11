60932705edb3a09099b3344817b77ab11803292f
a879 <57567445+a879@users.noreply.github.com>
2021-01-11 18:38:12 +0800

    json文件，防止ts js 同时打开报错    TS类型声明

---
+++ tsDome.js
@@ -0,0 +1,14 @@
+var isNumber = false;
+var num_1 = 123;
+num_1 = undefined;
+num_1 = null;
+// undefined null 是其它类型的  子类 
+var u;
+u = null;
+var n;
+n = undefined;
+var str_2 = "I am";
+var str_1 = "baxk " + num_1 + " , " + str_2 + " ";
+var notSure = "string";
+notSure = 465;
+console.log(str_1);
--- tsDome.ts
+++ tsDome.ts
@@ -1,1 +1,26 @@
-var dome: any ;
+let isNumber : boolean = false 
+
+let num_1 : number = 123
+
+num_1 = undefined
+num_1 = null
+
+
+// undefined null 是其它类型的  子类 
+
+let u : undefined ;
+u = null
+
+
+let n :null ;
+n = undefined
+
+let str_2 : string = "I am"
+
+let str_1 : string = `baxk ${num_1} , ${str_2} ` ;
+
+let notSure : any = "string"
+
+notSure = 465;
+
+console.log(str_1);
